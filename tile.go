0e1dedcbef587df44ae6ca5351fa411207443f53
guilledipa <guillermodp@google.com>
2022-09-04 21:21:55 +1000

    More fixes

--- tile.go
+++ tile.go
@@ -1,22 +1,26 @@
 package main
 
 import (
+	"image/color"
+
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 // Tile represents a space with an x,y coordinate within a Level.
 type Tile struct {
-	sprites []*ebiten.Image
+	sprite *ebiten.Image
 }
 
 // AddSprite adds a sprite to the Tile.
 func (t *Tile) AddSprite(s *ebiten.Image) {
-	t.sprites = append(t.sprites, s)
+	t.sprite = s
+}
+
+func (t *Tile) Paint(c color.RGBA) {
+	t.sprite.Fill(c)
 }
 
 // Draw draws the Tile on the screen using the provided options.
 func (t *Tile) Draw(screen *ebiten.Image, options *ebiten.DrawImageOptions) {
-	for _, s := range t.sprites {
-		screen.DrawImage(s, options)
-	}
+	screen.DrawImage(t.sprite, options)
 }
